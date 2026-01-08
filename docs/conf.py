c0a01d4243fa7ddd68eefdce14c45c7a36b06690
Daniel Haag <121057143+denialhaag@users.noreply.github.com>
2026-01-08 23:00:35 +0100

    Update license headers

--- docs/conf.py
+++ docs/conf.py
@@ -1,11 +1,3 @@
-# Copyright (c) 2023 - 2025 Chair for Design Automation, TUM
-# Copyright (c) 2025 Munich Quantum Software Company GmbH
-# All rights reserved.
-#
-# SPDX-License-Identifier: MIT
-#
-# Licensed under the MIT License
-
 """Sphinx configuration file."""
 
 from __future__ import annotations
@@ -32,9 +24,9 @@ version = metadata.version("mqt.ionshuttler")
 release = version.split("+")[0]
 
 project = "MQT IonShuttler"
-author = "Chair for Design Automation, TUM & Munich Quantum Software Company"
+author = "Chair for Design Automation, TUM"
 language = "en"
-project_copyright = "2023 - 2025 Chair for Design Automation, TUM & 2025 Munich Quantum Software Company"
+project_copyright = "2023 - 2026 Chair for Design Automation, TUM"
 
 master_doc = "index"
 
