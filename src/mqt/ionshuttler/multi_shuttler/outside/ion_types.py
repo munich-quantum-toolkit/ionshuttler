d0aa5b21078129d5142f267a74284a943b326bd2
Daniel Haag <121057143+denialhaag@users.noreply.github.com>
2026-08-17 18:34:34 +0200

    Add license headers

--- src/mqt/ionshuttler/multi_shuttler/outside/ion_types.py
+++ src/mqt/ionshuttler/multi_shuttler/outside/ion_types.py
@@ -1,2 +1,9 @@
+# Copyright (c) 2023 - 2026 Chair for Design Automation, TUM
+# All rights reserved.
+#
+# SPDX-License-Identifier: MIT
+#
+# Licensed under the MIT License
+
 Node = tuple[float, float]
 Edge = tuple[Node, Node]
--- src/mqt/ionshuttler/single_shuttler/main.py
+++ src/mqt/ionshuttler/single_shuttler/main.py
@@ -1,3 +1,10 @@
+# Copyright (c) 2023 - 2026 Chair for Design Automation, TUM
+# All rights reserved.
+#
+# SPDX-License-Identifier: MIT
+#
+# Licensed under the MIT License
+
 import random
 import sys
 import time
